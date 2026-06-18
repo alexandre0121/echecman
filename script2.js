ba89f824ad2416c0a9195e36587cd4e10a83035c
alexandre0121 <alexandre.wiche@ecolesrec.ch>
2026-06-18 16:26:02 +0200

    23

--- script2.js
+++ script2.js
@@ -1,33 +1,26 @@
 let click = 0;
 
 function color() {
-
   if (click % 2 == 0) {
     // mode sombre
     document.body.style.backgroundColor = "black";
     document.body.style.color = "white";
 
     let btn = document.getElementById("button");
-    
     btn.style.backgroundColor = "white";
     btn.style.color = "black";
     btn.textContent = "Noir";
     document.getElementById("click_me").style.color = "yellow";
-
-    click = click + 1;
-  } 
-  else {
+  } else {
     // mode clair
     document.body.style.backgroundColor = "white";
     document.body.style.color = "black";
 
     let btn = document.getElementById("button");
-    
     btn.style.backgroundColor = "black";
     btn.style.color = "white"; 
     btn.textContent = "Blanc";
     document.getElementById("click_me").style.color = "deeppink";
-
-    click = click + 1;
   }
+  click = click + 1;
 }
